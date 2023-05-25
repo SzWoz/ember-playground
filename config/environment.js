1222a07bce0089473368c275d5406e79b1055801
Szymon Woźny <szwoz2001@gmail.com>
2023-05-25 10:52:22 +0200

    change rootUrl

--- config/environment.js
+++ config/environment.js
@@ -4,7 +4,7 @@ module.exports = function (environment) {
   const ENV = {
     modulePrefix: 'ember-playground',
     environment,
-    rootURL: '/',
+    rootURL: '/ember-playground',
     locationType: 'history',
     EmberENV: {
       EXTEND_PROTOTYPES: false,
